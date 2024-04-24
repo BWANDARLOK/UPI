411bac06e4814e310f52bf00c1016be9416f4a34
OXYEGN <166620351+BWANDARLOK@users.noreply.github.com>
2024-04-24 14:40:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,7 @@ import json
 with open("app.json", "r") as file:
     config_data = json.load(file)
 
-BOT_TOKEN = config_data.get("7032675432:AAH_CnGQ_OL22Qih7z1WDWO19ELtr7NuH_o")
-CHANNEL_USERNAME = config_data.get("VIP_OXYGEN_STORE")
+BOT_TOKEN = config_data.get("bot_token")
+CHANNEL_USERNAME = config_data.get("channel_username")
+API_ID = config_data.get("api_id")
+API_HASH = config_data.get("api_hash")
