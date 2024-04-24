89d9a68dd99eb225b7a1c5bbf305cea4adc1e3d9
OXYEGN <166620351+BWANDARLOK@users.noreply.github.com>
2024-04-24 14:26:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import json
 with open("app.json", "r") as file:
     config_data = json.load(file)
 
-BOT_TOKEN = config_data.get("bot_token")
-CHANNEL_USERNAME = config_data.get("channel_username")
+BOT_TOKEN = config_data.get("7032675432:AAH_CnGQ_OL22Qih7z1WDWO19ELtr7NuH_o")
+CHANNEL_USERNAME = config_data.get("VIP_OXYGEN_STORE")
